132c5589624fc4db5b5fc6470e61faed7afe6995
oscrodsol <oscar43521@gmail.com>
2022-06-11 13:19:12 +0200

    Basic configuration of the project

---
+++ index.js
@@ -0,0 +1,27 @@
+
+
+const express = require('express');
+
+const app = express();
+
+const port = 3000;
+
+const db = require('./db/db');
+
+const router = require('./router.js');
+
+
+//Middlewares
+
+app.use(express.json());
+
+/* app.use(router); */
+
+
+//Conexión a base de datos y levantar servidor
+
+db.then(()=>{
+
+    app.listen(port, ()=> {console.log("Servidor levantado en el puerto ", port)});
+
+}).catch((err) => console.log(err.message));
